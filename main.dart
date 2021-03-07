a3e03c6dd4b7ea3626c875bed54c8adfaf14cd05
Irina Stefanescu <49494590+IrinaStefanescu@users.noreply.github.com>
2021-03-07 10:45:38 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: MagicBall(),
+      ),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+
+  int ballNumber = 1;
+  void _imageHandler(){
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Expanded(child: TextButton(
+                child: Image.asset('images/ball$ballNumber.png'),
+                onPressed: _imageHandler,
+              ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
